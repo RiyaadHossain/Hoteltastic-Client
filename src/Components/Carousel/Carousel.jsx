fc7aab6b637da9bbeac34df51f5de8b3cddf96c8
khanshorif331 <khanshorif331@gmail.com>
2022-09-06 01:07:20 +0600

    responsive issue solved

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -25,13 +25,27 @@ const Carousel = () => {
   effect={"coverflow"}
   grabCursor={true}
   centeredSlides={true}
-  slidesPerView={"auto"}
+  slidesPerView={1}
   coverflowEffect={{
     rotate: 50,
     stretch: 0,
     depth: 100,
     modifier: 1,
     slideShadows: true,
+  }}
+  breakpoints={{
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
   }}
   pagination={true}
   modules={[EffectCoverflow, Pagination]}
