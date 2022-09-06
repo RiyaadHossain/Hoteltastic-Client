82169202f99f0de79b274e66de5ff99972d2d101
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-09-06 18:51:56 +0600

    feature heading added

--- src/Pages/Home/FeatureRooms.jsx
+++ src/Pages/Home/FeatureRooms.jsx
@@ -1,10 +1,59 @@
+import { Box, Typography } from '@mui/material';
 import React from 'react';
 
 const FeatureRooms = () => {
      return (
-          <div >
-               
-          </div>
+          <Box sx={{
+               backgroundColor: "#e5f8ed",
+               textAlign: "center",
+               fontFamily: "'Rubik', sans-serif",
+               padding: "100px 0px 120px 0px"
+          }}>
+               <Typography
+                    sx={{
+                         fontSize: {
+                              xs: "38px",
+                              sm: "45px",
+                              md: "18px",
+                         },
+                         textTransform: "uppercase",
+                         fontFamily: "'Rubik', sans-serif",
+                         fontWeight: "500"
+
+                    }}
+                    fontWeight="bold"
+                    color="#2dbe6c"
+               >
+                    Features
+               </Typography>
+               <Typography
+                    sx={{
+                         fontWeight: "500",
+                         fontFamily: "'Rubik', sans-serif",
+                         fontSize: {
+                              xs: "38px",
+                              sm: "45px",
+                              md: "40px",
+                         },
+                    }}
+                    fontWeight="bold"
+               >
+                    Featured Property
+               </Typography>
+
+               <Typography
+                    sx={{
+                      maxWidth:"50%",
+                      margin:"0 auto"  
+                    }}
+                    color="#93959e"
+               >
+                    Lorem ipsum dolor sit amet consectetur adipisicing sed do eiusmod tempor incididunt
+                    labore dolore magna aliqua enim.
+               </Typography>
+
+
+          </Box>
      );
 };
 
--- src/Pages/Home/LandingPage.jsx
+++ src/Pages/Home/LandingPage.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import Banner from "./Banner";
+import FeatureRooms from "./FeatureRooms";
 
 function Home() {
-  return <><Banner/></>;
+  return <>
+            <Banner />
+            <FeatureRooms />
+         </>;
 }
 
 export default Home;
