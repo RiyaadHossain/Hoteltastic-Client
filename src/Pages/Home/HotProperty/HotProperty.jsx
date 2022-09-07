ab570c6be7a712e0d4f70ea557e66e6980f85eb1
khanshorif331 <khanshorif331@gmail.com>
2022-09-08 01:31:38 +0600

    content setup done

--- src/Pages/Home/HotProperty/HotProperty.jsx
+++ src/Pages/Home/HotProperty/HotProperty.jsx
@@ -1,14 +1,20 @@
-import { Box, Button, Card, CardActions, CardContent, styled, Typography } from '@mui/material';
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Grid, styled, Typography } from '@mui/material';
 import React from 'react';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import SyncAltIcon from '@mui/icons-material/SyncAlt';
+import HotelIcon from '@mui/icons-material/Hotel';
+import BathtubIcon from '@mui/icons-material/Bathtub';
+import Grid3x3Icon from '@mui/icons-material/Grid3x3';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import FlashOnIcon from '@mui/icons-material/FlashOn';
+import BeenhereIcon from '@mui/icons-material/Beenhere';
 
-
-const Container = styled(Box)`
-    margin:50px
-`
 const FlexContainer = styled(Box)`
     display:flex;
     justify-content:space-between;
     align-items:center;
+    margin:20px 0
 `
 
 const HotProperty = () => {
@@ -24,39 +30,234 @@ const HotProperty = () => {
         "description":"This kind of catergory are the best for personal uses"},
     ]
     return (
-        <Container>
-            <Typography variant='h6'>HOT PROPERTY</Typography>
+        <Box sx={{
+            margin:{
+            xs:"40px 10px",
+            sm:"60px 20px",
+            md:"100px 80px"
+        }}}>
+            
+            <Typography variant='h6' sx={{color:'#2dbe6c'}}>HOT PROPERTY</Typography>
             <FlexContainer>
                 <Typography variant='h4'>Our Best Deals</Typography>
                 <Box>
-                    <Button>Next</Button>
-                    <Button>Previus</Button>
+                    <Button sx={{
+                        border:'1px solid #93959e' ,
+                        color: "#fff",
+                        background: "#2dbe6c",
+                        padding: "12px 0",
+                        marginRight: "8px",
+                        boxShadow: "inset 0 0 0 0 #000",
+                        transition: "ease-out 0.2s",
+                        borderRadius: "5px",
+                        "&:hover": {
+                             color: "#fff",
+                             boxShadow: "inset 130px 0 0 0 #000",
+                             border: "2px solid #2dbe6c",
+                        }
+                        }}><ArrowBackIosNewIcon/></Button>
+                    <Button sx={{
+                        border:'1px solid #93959e' ,
+                        color: "#fff",
+                        background: "#2dbe6c",
+                        padding: "12px 0",
+                        marginRight: "8px",
+                        boxShadow: "inset 0 0 0 0 #000",
+                        transition: "ease-out 0.2s",
+                        borderRadius: "5px",
+                        "&:hover": {
+                             color: "#fff",
+                             boxShadow: "inset 130px 0 0 0 #000",
+                             border: "2px solid #2dbe6c",
+                        }
+                    }}><ArrowForwardIosIcon/></Button>
                 </Box>
             </FlexContainer>
-            <Box>
-            <Card sx={{ minWidth: 275 }}>
-                <CardContent>
-                <Typography sx={{ fontSize: 14 }} color="text.primary" gutterBottom>
-                    FEATURED
-                </Typography>
-                <Typography variant="h5" component="div">
-                be nev ent
-                </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                adjective
-                </Typography>
-                <Typography variant="body2">
-                well meaning and kindly.
-                <br />
-                {'"a benevolent smile"'}
-                </Typography>
-                </CardContent>
-                <CardActions>
-                    <Button size="small">See Datails</Button>
-                </CardActions>
-                </Card>
+            {/* card starts here */}
+            <Grid container rowSpacing={5} columnSpacing={{ xs: 1, sm: 2, md: 5 }}> 
+                {/* first grid item */}
+                <Grid item xs={12} sm={12} md={6} >
+                <Card sx={{
+                    width: '100%',
+                    height:{
+                        sm:'400px',
+                        md:'400px'
+                    },
+               borderRadius:"10px"
+          }}
+               fontFamily="'Rubik', sans-serif"
+          >     
+                <Box sx={{
+                display:"flex",
+                justifyContent:"space-between",
+                padding: "15px 30px"
+            }}>
+                <Typography sx={{
+                    color:'#fff',
+                    background: '#f2b241',
+                    padding:'5px 10px',
+                    fontWeight: 'bold',
+                    borderRadius: '5px'
+                }}>FEATURED</Typography>
+                <Typography component='span'><BeenhereIcon sx={{
+                    color:'#2dbe6c',
+                    fontSize:'30px',
+                }}/></Typography>
             </Box>
-        </Container>
+              
+               <CardContent sx={{
+                    textAlign: "left",
+                    padding: "20px 30px 40px 30px"
+               }}>
+                    <Typography
+                         gutterBottom
+                         variant="h5"
+                         component="div"
+                         color="#2d2929"
+                         fontSize={{
+                              md: 22
+                         }}
+                         fontWeight="500"
+                    >
+                         Contemporary Apartment
+                    </Typography>
+                    <Box sx={{ 
+                         display: "flex",
+                         flexWrap: "wrap",
+                         justifyContent: "center",
+                     }}>
+                         <Box sx={{ width: "50%" }}>
+                              <Typography sx={{
+                                   fontWeight: 500,
+                                   color: "#93959e"
+                              }}
+                              > 
+                                   Start From
+                              </Typography>
+                              <Typography sx={{
+                                   fontWeight: "500",
+                                   color: "#2dbe6c",
+                                   fontSize: {
+                                        md: 22
+                                   }
+                              }}>
+                                   price
+                              </Typography>
+                         </Box>
+                         <Box sx={{ width: "50%", textAlign: "right" }}>
+
+                              <SyncAltIcon
+                                   sx={{
+                                        border: "1px solid #e5e7ec",
+                                        padding: "3px",
+                                        height: "40px",
+                                        width: "40px",
+                                        color: "#a6a7af",
+                                        borderRadius: "4px",
+                                        transition: "ease-out 0.2s",
+                                        "&:hover": {
+                                            color:"#fff",
+                                            background: "#2dbe6c",
+                                            boxShadow: "inset 130px 0 0 0 #2dbe6c",
+                                            border: "2px solid #2dbe6c",
+                                            cursor: "pointer"
+                                       }
+                                   }}
+                              />
+                              <FavoriteBorderIcon
+                                   sx={{
+                                        border: "1px solid #e5e7ec",
+                                        padding: "3px",
+                                        height: "40px",
+                                        width: "40px",
+                                        color: "#a6a7af",
+                                        borderRadius: "4px",
+                                        marginLeft: "5px",
+                                        transition: "ease-out 0.2s",
+                                        "&:hover": {
+                                            color:"#fff",
+                                            background: "#2dbe6c",
+                                            boxShadow: "inset 130px 0 0 0 #2dbe6c",
+                                            border: "2px solid #2dbe6c",
+                                            cursor: "pointer"
+                                       }
+                                   }}
+                              />
+                         </Box>
+                    </Box>
+                    <Typography variant="body2" color="#93959e" margin="10px 0">
+                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore deserunt illum, dolores magnam autem natus maiores error nemo numquam tenetur.
+                    </Typography>
+                    <Box sx={{
+                        display:'flex',
+                        justifyContent:'space-between',
+                        alignItems:'center',
+                        padding : '15px 0',
+                        marginBottom:'10px',
+                        height:'50px',
+                    }}>
+                        <Box sx={{display:'flex',alignItems:'center'}}>
+                            <Box component='span' sx={{marginRight:'6px'}}>
+                                <HotelIcon/>
+                            </Box>
+                            <Typography>
+                            Beds
+                            </Typography>
+                        </Box>
+                        <Typography>|</Typography>
+                        <Box sx={{display:'flex',alignItems:'center'}}>
+                            <Box component='span' sx={{marginRight:'6px'}}>
+                                <BathtubIcon/>
+                            </Box>
+                            <Typography>
+                            Baths
+                            </Typography>
+                        </Box>
+                        <Typography>|</Typography>
+                        <Box sx={{display:'flex',alignItems:'center'}}>
+                            <Box component='span' sx={{marginRight:'6px'}}>
+                                <Grid3x3Icon/>
+                            </Box>
+                            <Typography>
+                            Area
+                            </Typography>
+                        </Box>
+                    </Box>
+                    <Button sx={{
+                         color: "#fff",
+                         background: "#2dbe6c",
+                         border: "1px solid #e5e7ec",
+                         padding: "10px 0px",
+                         width: "130px",
+                         boxShadow: "inset 0 0 0 0 #000",
+                         transition: "ease-out 0.5s",
+                         borderRadius: "5px",
+                         "&:hover": {
+                              color: "#fff",
+                              boxShadow: "inset 130px 0 0 0 #000",
+                              border: "2px solid #2dbe6c",
+                         }
+
+                    }} >See Details</Button>
+               </CardContent>
+          </Card>
+                
+            </Grid>
+            
+          <Grid item xs={12} sm={12} md={6} >
+          <Box sx={{
+            width:'100%',
+            height: {
+                sm: '400px',
+                md: '400px',
+
+            }
+          }}>
+            <img style={{borderRadius:10,width:'100%',height:'100%'}}  src="http://azim.commonsupport.com/Realshed/assets/images/resource/deals-1.jpg" alt="" />
+          </Box>
+          </Grid>
+            </Grid>
+        </Box>
     );
 };
 
--- src/Pages/Home/LandingPage.jsx
+++ src/Pages/Home/LandingPage.jsx
@@ -5,12 +5,14 @@ import AppPromotion from "./AppPromotion";
 import CallToAction from "./CallToAction";
 import FeatureRooms from "./FeatureRooms";
 import NewsArticle from "./NewsArticle";
+import HotProperty from "./HotProperty/HotProperty";
 
 function Home() {
   return (
     <>
       <Banner />
       <FeatureRooms />
+      <HotProperty/>
       <CallToAction />
       <NewsArticle/>
       <AppPromotion />
