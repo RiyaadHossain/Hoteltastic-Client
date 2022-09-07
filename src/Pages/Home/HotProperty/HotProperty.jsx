26fb237f5f73c70100bd488684b67eaec2b26b5a
khanshorif331 <khanshorif331@gmail.com>
2022-09-07 21:32:09 +0600

    uupdate done

--- src/Pages/Home/HotProperty/HotProperty.jsx
+++ src/Pages/Home/HotProperty/HotProperty.jsx
@@ -1,16 +1,61 @@
-import { Box,styled, Typography } from '@mui/material';
+import { Box, Button, Card, CardActions, CardContent, styled, Typography } from '@mui/material';
 import React from 'react';
 
 
-const Container =styled(Box)`
-    margin-top: 100px;
-    margin-bottom:100px;
+const Container = styled(Box)`
+    margin:50px
+`
+const FlexContainer = styled(Box)`
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
 `
 
 const HotProperty = () => {
+    const dealsData = [
+        {"name":"Contemporary Apartment",
+        "price": 45000,
+        "description":"This kind of catergory are the best for personal uses"},
+        {"name":"Luxury Villa With Pool",
+        "price": 45000,
+        "description":"This kind of catergory are the best for personal uses"},
+        {"name":"Contemporary Apartment",
+        "price": 45000,
+        "description":"This kind of catergory are the best for personal uses"},
+    ]
     return (
         <Container>
-            <Typography>HOT PROPERTY</Typography>
+            <Typography variant='h6'>HOT PROPERTY</Typography>
+            <FlexContainer>
+                <Typography variant='h4'>Our Best Deals</Typography>
+                <Box>
+                    <Button>Next</Button>
+                    <Button>Previus</Button>
+                </Box>
+            </FlexContainer>
+            <Box>
+            <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.primary" gutterBottom>
+                    FEATURED
+                </Typography>
+                <Typography variant="h5" component="div">
+                be nev ent
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                adjective
+                </Typography>
+                <Typography variant="body2">
+                well meaning and kindly.
+                <br />
+                {'"a benevolent smile"'}
+                </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">See Datails</Button>
+                </CardActions>
+                </Card>
+            </Box>
         </Container>
     );
 };
