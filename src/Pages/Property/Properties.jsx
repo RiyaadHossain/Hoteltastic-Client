9a29315a7af0cf8d63809df6f7ea6d58a6970b89
khanshorif331 <khanshorif331@gmail.com>
2022-09-10 23:56:25 +0600

    propery details component done

--- src/Pages/Property/Properties.jsx
+++ src/Pages/Property/Properties.jsx
@@ -1,10 +1,37 @@
 import { Box, Typography } from '@mui/material';
 import React from 'react';
+import Background from '../../Assets/Background/callToActionBg.png'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 const Properties = () => {
     return (
-        <Box sx={{color:'#000',height:'100vh'}}>
-            <Typography>Hello property</Typography>
+        <Box sx={{height:'100vh',fontFamily: "'Rubik', sans-serif",}}>
+            <Box sx={{
+                width:'100%',
+                height:'15rem',
+                background:`rgba(229, 248, 237,0.9) url(${Background}) no-repeat`,
+                display:'flex',
+                flexDirection:'column',
+                justifyContent:'center',
+                alignItems:'center',
+            }}>
+                <Typography sx={{
+                    fontSize:{
+                        xs:'20px',
+                        sm:'30px',
+                        md:'40px',
+                    },
+                    marginBottom:'10px',
+                    fontWeight:500,
+                }}>Property Details 01</Typography>
+                <Typography>Home 
+                <ChevronRightIcon sx={{fontSize:14,mx:1}}/> 
+                <Typography component='span' sx={{
+                color:'#2dbe6c',
+                fontWeight:500,
+                }}>Property Details 01</Typography>
+                </Typography>
+            </Box>
         </Box>
     );
 };
