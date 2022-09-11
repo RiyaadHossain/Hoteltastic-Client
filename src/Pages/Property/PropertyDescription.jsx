9eb9f60a251d22984a8e02a341ce1445ed9c8d29
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-09-11 14:26:55 +0600

    complete property desciption and details

--- src/Pages/Property/PropertyDescription.jsx
+++ src/Pages/Property/PropertyDescription.jsx
@@ -1,26 +1,42 @@
-import { Typography } from '@mui/material';
+import { Card, CardActionArea, CardContent, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
 
 const PropertyDescription = () => {
      return (
-          <Box>
+          <Box sx={{
+               padding: "43px 30px",
+               marginBottom: "30px",
+               backgroundColor: "#ffffff",
+               boxShadow: "0 10px 30px 0px rgb(0 0 0 / 10%)",
+               borderRadius: "10px",
+          }}>
                <Typography
-				sx={{
-					fontWeight: '500',
-					fontFamily: "'Rubik', sans-serif",
-					fontSize: {
-						xs: '38px',
-						sm: '45px',
-						md: '40px',
-					},
-				}}
-				fontWeight="bold"
-			>
-				Property Desciption
-			</Typography>
-               
+                    sx={{
+                         fontWeight: '500',
+                         fontFamily: "'Rubik', sans-serif",
+                         borderBottom: "2px solid #e5e7ec",
+                         paddingBottom: "10px",
+                         fontSize: {
+                              md: '22px',
+                         },
+                    }}
+               // fontWeight="bold"
+               >
+                    Property Desciption
+               </Typography>
+               <Typography sx={{
+                    borderTop: "2px solid #e5e7ec",
+                    paddingTop: "10px",
+                    marginTop: "3px",
+               }}
+                    variant="body2" color="text.secondary">
+                    Lorem ipsum dolor sit amet consectetur adipisicing sed eiusm tempor incididunt labore dolore magna aliqua enim minim veniam quis nostrud exercitation laboris nisi ut aliquip ea commodo consequat duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur occaecat, <br />
+
+                    Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim est laborum. Sed perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium totam rem aperiam.
+               </Typography>
           </Box>
+
      );
 };
 
