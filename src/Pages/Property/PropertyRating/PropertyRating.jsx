38be5c17b4933d4cf355cadcd48bb6dc3ade16ab
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-09-12 18:11:57 +0600

    add review section done

--- src/Pages/Property/PropertyRating/PropertyRating.jsx
+++ src/Pages/Property/PropertyRating/PropertyRating.jsx
@@ -1,15 +1,30 @@
-import { Box, Rating, Typography } from '@mui/material';
+import { Box, Button, Modal, Rating, TextField, Typography } from '@mui/material';
 import axios from 'axios';
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 
+
+const style = {
+     position: 'absolute',
+     top: '50%',
+     left: '50%',
+     transform: 'translate(-50%, -50%)',
+     width: "50%",
+     bgcolor: 'background.paper',
+     boxShadow: 24,
+     p: 4,
+};
 const PropertyRating = () => {
      const [reviews, setReviews] = useState([])
      useEffect(() => {
           axios.get(`userReviews.json`).then(data => setReviews(data.data))
      }, [])
      // console.log(reviews);
+
+     const [open, setOpen] = React.useState(false);
+     const handleOpen = () => setOpen(true);
+     const handleClose = () => setOpen(false);
      return (
           <Box sx={{
                padding: "22px 30px",
@@ -18,6 +33,78 @@ const PropertyRating = () => {
                boxShadow: "0 10px 30px 0px rgb(0 0 0 / 10%)",
                borderRadius: "10px",
           }}>
+
+               <div>
+                    <Button onClick={handleOpen}>Add Review</Button>
+                    <Modal
+                         open={open}
+                         onClose={handleClose}
+                         aria-labelledby="modal-modal-title"
+                         aria-describedby="modal-modal-description"
+                    >
+                         <Box sx={{
+                              position: 'absolute',
+                              top: '50%',
+                              left: '50%',
+                              transform: 'translate(-50%, -50%)',
+                              width: "40%",
+                              bgcolor: 'background.paper',
+                              boxShadow: 24,
+                              p: "40px 70px",
+                              borderRadius: "10px"
+                         }}>
+                              <Typography sx={{
+                                   fontSize: "30px",
+                                   fontWeight: "600",
+                                   marginBottom: "10px",
+                                   borderBottom: "1px solid white",
+                              }}>
+                                   Review
+                              </Typography>
+                              <form >
+                                   <TextField
+                                        sx={{
+                                             background: "none",
+                                             width: "100%",
+                                             margin: "8px 0px 10px 0px",
+                                        }}
+                                        id="outlined-basic" label="Name" variant="outlined"
+                                        multiline
+                                   />
+                                   <TextField
+                                        sx={{
+                                             background: "none",
+                                             width: "100%",
+                                             margin: "8px 0px 10px 0px",
+                                        }}
+                                        id="outlined-multiline-static"
+                                        label="Comment"
+                                        multiline
+                                        rows={4}
+                                   />
+
+                                   <Rating sx={{my:"10px"}} name="half-rating" defaultValue={0} precision={0.5} />
+
+                                   <Button variant='outlined' sx={{
+                                        background: "#2dbe6c",
+                                        color: "white",
+                                        border: "1px solid #2dbe6c",
+                                        margin: "8px 0px 10px 0px",
+                                        fontWeight: "600",
+                                        padding: "10px 0",
+                                        width: "100%",
+                                        ":hover": {
+                                             background: "white",
+                                             border: "1px solid #2dbe6c",
+                                             color: "#2dbe6c",
+                                        }
+
+                                   }}>Submit</Button>
+                              </form>
+                         </Box>
+                    </Modal>
+               </div>
+
                {
                     reviews.map(review => {
                          return (
@@ -25,9 +112,9 @@ const PropertyRating = () => {
                                    display: "flex",
                                    color: "#93959e",
                                    py: "15px",
-                                   borderTop:"1px solid #e5e7ec",
-                                   ":first-child":{
-                                   borderTop:"none",
+                                   borderBottom: "1px solid #e5e7ec",
+                                   ":last-child": {
+                                        borderBottom: "none",
                                    }
                               }}>
                                    <img style={{
@@ -39,7 +126,7 @@ const PropertyRating = () => {
                                    <Box >
                                         <Box sx={{
                                              display: "flex",
-                                             alignItems:"center"
+                                             alignItems: "center"
                                         }}>
                                              <Typography
                                                   sx={{
@@ -48,8 +135,8 @@ const PropertyRating = () => {
                                                        },
                                                        fontFamily: "'Rubik', sans-serif",
                                                        // fontWeight: "500",
-                                                       mr:"10px",
-                                                       color:"#626263"
+                                                       mr: "10px",
+                                                       color: "#626263"
 
                                                   }}
                                              >
@@ -60,9 +147,6 @@ const PropertyRating = () => {
                                         </Box>
                                         <Typography
                                              sx={{
-                                                  fontSize: {
-                                                       // md: "18px",
-                                                  },
                                                   fontFamily: "'Rubik', sans-serif",
                                              }}
                                         >
@@ -74,6 +158,8 @@ const PropertyRating = () => {
                          )
                     })
                }
+
+
           </Box>
      );
 };
