89cedee58cb99abc7a35c56356b75e46f1016244
NishatFariza <nishatfarzia78@gmail.com>
2022-09-19 05:53:13 +0600

    about project banner added

---
+++ src/Pages/AboutProject/Technology.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Technology = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Technology;
--- src/Pages/AboutProject/TechnologyUsed.jsx
+++ src/Pages/AboutProject/TechnologyUsed.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import { Box, Typography } from "@mui/material";
+import React from "react";
 
 const TechnologyUsed = () => {
-    return (
-        <div>
+  return (
+    <>
+      <Box
+        sx={{
+          fontFamily: "'Rubik', sans-serif",
+          padding: "64px 70px 63px 70px",
+          color: "#000",
+          width: "90%",
+          margin: "0 auto",
+        }}
+      >
+        <Typography
+          sx={{
+            fontWeight: "500",
+            fontFamily: "'Rubik', sans-serif",
+            fontSize: {
+              xs: "28px",
+              sm: "30px",
+              md: "34px",
+              borderBottom: "",
+            },
+          }}
+          fontWeight="bold"
+        >
+          Used Technology
+          <Box
+            sx={{
+              width: "25%",
+              background:
+                "linear-gradient(90deg, hsl(0, 0%, 70%), hsl(0, 0%, 20%), hsl(0, 0%, 70%))",
+              height: "2px",
+            }}
+          ></Box>
+        </Typography>
+        <Box>
             
-        </div>
-    );
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default TechnologyUsed;
