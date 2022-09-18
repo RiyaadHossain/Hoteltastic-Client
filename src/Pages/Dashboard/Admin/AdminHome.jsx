3250fba98a5ffcb769b138c7180833cda24fcffe
khanshorif331 <khanshorif331@gmail.com>
2022-09-19 01:21:07 +0600

    card finalize done

--- src/Pages/Dashboard/Admin/AdminHome.jsx
+++ src/Pages/Dashboard/Admin/AdminHome.jsx
@@ -34,13 +34,16 @@ function AdminHome() {
       }
     }}>
       <Grid container spacing={4}>
-        <Grid item xs={12} sm={6} md={4} lg={4}>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
           <CardForDashboard />
         </Grid>  
-        <Grid item xs={12} sm={6} md={4} lg={4}>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
           <CardForDashboard />
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={4}>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <CardForDashboard />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
           <CardForDashboard />
         </Grid>
       </Grid>
