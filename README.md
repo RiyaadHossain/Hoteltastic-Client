8c8b8c8614be0812ad23b831e1900af5c4ae7a40
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-10-18 20:59:22 +0600

    .md and about page updated

--- README.md
+++ README.md
@@ -7,15 +7,22 @@ admin can make any user admin even he can ban any user.
 
 ##### Live Site: https://hoteltastic.netlify.app/
 
+##### Server Side: https://github.com/RiyaadHossain/Hoteltastic-Server
+
+
 <br>
 
 ### Features:
 
-<li>MVC Architechture Pattern</li>
-<li>JWT Based Authentication</li>
-<li>Role Based Authorization</li>
-<li>Admin & User Dashboard</li>
-<li>Multer to upload File </li>
+<li>Design the file structure following MVC Architechture Pattern</li>
+<li>Authenticate user implementing JWT Based Authentication</li>
+<li>After Authentication, user can pay and book any desired room</li>
+<li>For Authorization, implemented Role Based Authorization</li>
+<li>Have protected routes which can be navigated by admin only</li>
+<li>Moreover, admin can make admin any user and even can ban</li>
+<li>Additionaly, admin can create-update-delete any room</li>
+<li>Have functionality for Admin & User Dashboard</li>
+<li>Admin can post room photo using Multer </li>
 
 <br>
 
@@ -24,12 +31,18 @@ admin can make any user admin even he can ban any user.
 <li>HTML</li>
 <li>CSS</li>
 <li>JavaScript</li>
+<li>React</li>
+<li>React Hook</li>
+<li>Functional Component</li>
+<li>React Hook Form</li>
 <li>Redux</li>
+<li>Axios</li>
 <li>MUI</li>
 <li>Node</li>
 <li>Express</li>
 <li>MongoDB</li>
 <li>Mongoose</li>
+<li>Bcrypt</li>
 <li>JWT</li>
 <li>Multer</li>
 <li>MVC</li>
--- src/Pages/AboutProject/FeatureImplement.jsx
+++ src/Pages/AboutProject/FeatureImplement.jsx
@@ -37,7 +37,7 @@ const FeatureImplement = () => {
         ></Box>
       </Typography>
       <Box ml={5}>
-        <Typography width={880} mb={4}>
+        <Typography maxWidth={880} mb={4}>
           Hoteltastic is a hotel management site where user can visit and book
           any exsiting room after successfully log in. Also, admin can track
           their user authorities, add-update-delete any exsiting room. Moreover,
@@ -48,25 +48,34 @@ const FeatureImplement = () => {
             Key Features:
           </span>
           <ul style={{ marginLeft: 35, marginTop: 5 }}>
-            <li>MVC Architechture Pattern</li>
-            <li>JWT Based Authentication</li>
-            <li>Role Based Authorization</li>
-            <li>Admin & User Dashboard</li>
-            <li>Multer to upload File </li>
+            <li>Design the file structure following MVC Architechture Pattern</li>
+            <li>Authenticate user implementing JWT Based Authentication</li>
+            <li>After Authentication, user can pay and book any desired room</li>
+            <li>For Authorization, implemented Role Based Authorization</li>
+            <li>Have protected routes which can be navigated by admin only</li>
+            <li>Moreover, admin can make admin any user and even can ban</li>
+            <li>Additionaly, admin can create-update-delete any room</li>
+            <li>Have functionality for Admin & User Dashboard</li>
+            <li>Admin can post room photo using Multer </li>
           </ul>
         </Typography>
-        <Box display='flex' gap={1} >
+        <Box display='flex' flexWrap='wrap' gap={1} >
           <Chip label="HTML" variant="outlined" color="info" size="small" />
           <Chip label="CSS" variant="outlined" color="info" size="small" />
           <Chip label="JavaScript" variant="outlined" color="info" size="small" />
           <Chip label="React" variant="outlined" color="info" size="small" />
+          <Chip label="React Hook" variant="outlined" color="info" size="small" />
+          <Chip label="Fucntinal Component" variant="outlined" color="info" size="small" />
+          <Chip label="React Hook Form" variant="outlined" color="info" size="small" />
           <Chip label="Redux" variant="outlined" color="info" size="small" />
+          <Chip label="Axios" variant="outlined" color="info" size="small" />
           <Chip label="MUI" variant="outlined" color="info" size="small" />
           <Chip label="Node" variant="outlined" color="info" size="small" />
           <Chip label="Express" variant="outlined" color="info" size="small" />
           <Chip label="MongoDB" variant="outlined" color="info" size="small" />
           <Chip label="Mongoose" variant="outlined" color="info" size="small" />
           <Chip label="JWT" variant="outlined" color="info" size="small" />
+          <Chip label="Bcrypt" variant="outlined" color="info" size="small" />
           <Chip label="Multer" variant="outlined" color="info" size="small" />
           <Chip label="MVC" variant="outlined" color="info" size="small" />
         </Box>
